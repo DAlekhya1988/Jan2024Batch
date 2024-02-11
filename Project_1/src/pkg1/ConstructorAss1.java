e9a414d686fa94161f7c209267db1f8d08bf8a89
HP <HP@host.docker.internal>
2024-02-11 23:10:33 +0100

    sharing the code first time

---
+++ Project_1/src/pkg1/ConstructorAss1.java
@@ -0,0 +1,35 @@
+package pkg1;
+
+public class ConstructorAss1 
+{
+     public ConstructorAss1() 
+ 	{
+    	this(12,23,67);
+ 		System.out.println("Default Constructor");
+ 	}
+     public ConstructorAss1(int a)
+     {
+     	 this();   	
+    	 System.out.println("This is a one parameterized constructor");
+     }
+	   public ConstructorAss1(int a,int b)
+	{
+		this(34);   
+		System.out.println("This is a two parameterized constructor");
+	}
+	   public ConstructorAss1(int a,int b, int c, int d)
+	   {
+		   this(1,2);
+		   System.out.println("This is a Four parameterized constructor");
+	   }
+	
+	public ConstructorAss1(int i, int j, int k) 
+	{
+		System.out.println("This is three parameterized constructor");
+	}
+	public static void main(String[] args) 
+	{
+	  ConstructorAss1 ob=new ConstructorAss1(200,220,260,280);
+	}
+
+}
---
+++ Project_1/src/pkg1/Print.java
@@ -0,0 +1,11 @@
+package pkg1;
+
+public class Print {
+
+	public static void main(String args[])
+	{
+		 System.out.println(" +\"\"\"\"\"+ ");
+		 System.out.println("[| o o |]");
+		 System.out.println(" |  ^  | ");
+	}
+}
